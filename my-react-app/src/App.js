c82c41ee610433c0c347c5985ebef163f18257b1
ASUS <ngocanh119001@gmail.com>
2022-08-26 21:30:44 +0700

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+function App() {
+
+    return ( < div class = "flex-container" >
+        < div > 1 </div> 
+        < div > 2 </div> 
+        <div > 3 </div>   
+        <div > 4 </div>  
+        <div > 5 </div> 
+        <div > 6 </div>   
+        <div > 7 </div> 
+        <div > 8 </div> 
+        </div>);
+
+    }
+export default App;
